61ab7054cc0980727d56cfd5a5ca3e8b368c7fef
ilyatfi <ilya05@inbox.lv>
2023-08-08 16:25:27 +0300

    links now work with clear query too

--- functions.php
+++ functions.php
@@ -4,6 +4,12 @@ function getNewUrl(string $url, string $route)
 {
     $uri = parse_url($_SERVER['REQUEST_URI']);
 
-    $new_url = str_replace($uri['query'], 'r='.$route, $url);
+    if (empty($uri['query'])) {
+        $new_url = $url.'?r='.$route;
+    }
+    else {
+        $new_url = str_replace($uri['query'], 'r='.$route, $url);
+    }
+
     return $new_url;
 }
