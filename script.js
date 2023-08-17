47d54b93bd3a4eb29e435183224e3613b9b42eb0
ilyatfi <ilya05@inbox.lv>
2023-08-17 16:03:20 +0300

    products handler callback function

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-const card_wrapper = document.querySelector("#catalog .card-wrapper");
+const fetchProducts = async (callback) => {
+    const response = await fetch("products.json");
+    const products = await response.json();
+    console.log(products);
+    callback(products)
+}
 
-function printProducts(products) {
+const productsHandler = (products) => {
+
+    const card_wrapper = document.querySelector("#catalog .card-wrapper");
+
     for (let i = 0; i < products.length; i++) {
 
         const card = document.createElement("div");
         card.classList.add("card");
 
-        const name_par = document.createElement("p");
-        name_par.classList.add("name");
+        const name_span = document.createElement("span");
+        name_span.classList.add("name");
         const name = document.createTextNode(products[i].name);
-        name_par.appendChild(name);
+        name_span.appendChild(name);
 
-        const price_par = document.createElement("p");
-        price_par.classList.add("price");
+        const price_span = document.createElement("span");
+        price_span.classList.add("price");
         const price = document.createTextNode(products[i].price);
-        price_par.appendChild(price);
+        price_span.appendChild(price);
 
-        const quantity_par = document.createElement("p");
-        quantity_par.classList.add("quantity");
+        const quantity_span = document.createElement("span");
+        quantity_span.classList.add("quantity");
         const quantity = document.createTextNode("Quantity: "+products[i].quantity);
-        quantity_par.appendChild(quantity);
+        quantity_span.appendChild(quantity);
 
-        card.appendChild(name_par);
-        card.appendChild(price_par);
-        card.appendChild(quantity_par);
-        card_wrapper.appendChild(card);
+        card.appendChild(name_span);
+        card.appendChild(price_span);
+        card.appendChild(quantity_span);
+        card_wrapper.appendChild(card)
     }
 }
 
-async function fetchProducts() {
-    const response = await fetch("products.json");
-    const products = await response.json();
-    console.log(products);
-    printProducts(products);
-}
-
-fetchProducts();
+fetchProducts(productsHandler);
