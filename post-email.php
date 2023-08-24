6df3f62a4776e5d7777ee3f930b873e7b11c26b7
ilyatfi <ilya05@inbox.lv>
2023-08-24 19:23:06 +0300

    emails checked to be unrepeated

--- post-email.php
+++ post-email.php
@@ -2,26 +2,26 @@
 
 if(isset($_POST['subscribtion'])) {
 
-    if(file_exists("emails.json")) {
-        
+    if(file_exists("emails.json"))
         $emails = json_decode(file_get_contents("emails.json"));
-    }
-    else {
-        $emails = [];
-    }
 
+    if(!is_array($emails))
+        $emails = [];
+    
     $email = $_POST['email'];
 
-    if(is_array($emails)) {
+    $invalid = false;
 
-        array_push($emails, $email);
-    }
-    else {
-        $emails = [];
-        array_push($emails, $email);
-    }
+    if($email == "")
+        $invalid = true;
 
-    //TODO: compare if there is no already added that email
+    foreach ($emails as $value) {
+        if($value == $email)
+            $invalid = true;
+    }
+    if(!$invalid)
+        array_push($emails, $email);
+
     file_put_contents("emails.json", json_encode($emails));
     header("location:javascript://history.go(-1)");
 }
